b335eeb5ef25d849f6ae7d8be5548f12d8989e5c
Xie YongSheng <xietbro@163.com>
2024-08-05 19:55:52 +0800

    clap crate命令行结构化与属性宏定义学习，serde序列化与反序列化学习

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use rcli::opts::{Opts, SubCommand};
+use rcli::process::process_csv;
+// rcli csv -i input.csv -o output.json --header -d ','
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => {
+            process_csv(&opts.input, &opts.output)?;
+        }
+    }
+    Ok(())
 }
