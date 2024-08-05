956f682c0bffdf16640107ce0e2b5283999a3539
Xie YongSheng <xietbro@163.com>
2024-08-05 23:01:14 +0800

    根据csv不同字段实现自动化解析-序列化-反序列化为json、yaml格式

--- src/opts.rs
+++ src/opts.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
+use std::fmt;
 use std::path::Path;
+use std::str::FromStr;
 
 #[derive(Debug, Parser)]
 #[command(name="rcli",version,author,about,long_about=None)]
@@ -12,14 +14,25 @@ pub enum SubCommand {
     #[command(name = "csv", about = "CSV to Others")]
     Csv(CsvOpts),
 }
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short,long,value_parser=verify_input_file)]
     pub input: String,
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+
+    #[arg(short, long)]
+    pub output: Option<String>,
+
+    #[arg(long, value_parser=parse_format, default_value = "json")]
+    pub format: OutputFormat,
+
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
+
     #[arg(long, default_value_t = true)]
     pub header: bool,
 }
@@ -35,4 +48,30 @@ fn verify_input_file(filename: &str) -> Result<String, &'static str> {
         return Err("Input file does not exist");
     }
     Ok(filename.into())
+}
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse::<OutputFormat>()
+}
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow::anyhow!("Invalid output format")),
+        }
+    }
+}
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
+    }
 }
--- src/process.rs
+++ src/process.rs
@@ -1,3 +1,4 @@
+use crate::opts::OutputFormat;
 use anyhow::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
@@ -15,15 +16,23 @@ struct Player {
     kit: u8,
 }
 
-pub fn process_csv(input: &str, output: &str) -> Result<()> {
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
-
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        let json_value = headers
+            .iter()
+            .zip(record.iter())
+            .collect::<serde_json::Value>();
+        ret.push(json_value);
     }
-    let json = serde_json::to_string_pretty(&ret)?;
-    fs::write(output, json)?;
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+    };
+
+    fs::write(output, content)?;
     Ok(())
 }
